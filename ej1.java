055877b6d3a8f04dac802a36e783fe0081d09074
JaviOvi <106032666+javiovi@users.noreply.github.com>
2023-01-10 18:56:54 -0300

    Add files via upload

---
+++ ej1.java
@@ -0,0 +1,18 @@
+package Objetos;
+
+public class ej1 {
+public static void main(String[] args) {
+
+    System.out.println("Hola Java");
+
+    System.out.println(1);
+
+    System.out.println(false);
+    System.out.println(1.5);
+    System.out.println('a');
+    System.out.println(4.9f);
+    System.out.println(3.4d);
+    System.out.println(4567
+    );
+}
+}
