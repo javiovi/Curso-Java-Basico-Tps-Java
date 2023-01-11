7f276af783ed1bec3fe56a7e387d2f4ac8a31b8d
JaviOvi <106032666+javiovi@users.noreply.github.com>
2023-01-11 15:01:50 -0300

    Add files via upload

---
+++ ej2.java
@@ -0,0 +1,22 @@
+package Objetos;
+
+import java.util.Scanner;
+
+
+public class ej2 {
+
+    public static void main(String[] args) {
+        
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Introduce un numero:  ");
+
+        int numero = scanner.nextInt();
+
+        double iva = numero * 0.21;
+        double suma = numero + iva;
+
+        System.out.println("El resultado con IVA  es: " + suma);
+
+    }
+}
