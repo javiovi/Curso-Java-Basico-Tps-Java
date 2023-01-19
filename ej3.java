42a7003f88e5eb4ee7b210735566a2e3cbc1d14e
JaviOvi <106032666+javiovi@users.noreply.github.com>
2023-01-19 17:58:10 -0300

    Ejercicio 3 Curso JAva Basico
    En este ejercicio tenéis que crear un bucle que permita concatenar textos e imprima el resultado final por consola.
    
    Tened en cuenta que los textos pueden venir de un array de tipo String. Por ejemplo:
    
    String[] nombres = {"", "", "", ""}

---
+++ ej3.java
@@ -0,0 +1,15 @@
+
+
+public class ej3 {
+    
+    public static void main(String[] args) {
+
+    String[] perros = { "Leia", "More", "Titi"};
+
+    for(int i = 0; i < perros.length; i++) {
+        System.out.println(perros[i]);
+    }
+
+
+    }
+}
